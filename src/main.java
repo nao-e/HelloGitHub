04ef67989d2f1902928352bf856c73c44f8de983
nao-e <nao-e@192.168.100.129>
2017-06-16 16:40:38 +0900

    code add

--- src/main.java
+++ src/main.java
@@ -3,5 +3,12 @@ public class main {
 
 	public static void main(String[] args){
 		System.out.println("これは、サンプルプログラムです。");
+		printNum();
+	}
+
+	public static void printNum(){
+		for(int i = 0 ; i < 10 ; i++){
+			System.out.println("i == " + i);
+		}
 	}
 }
