9a2b2fb2c64ff0ca5bc24421f6f19d8af6cceeda
nao-e <nao-e@192.168.100.129>
2017-06-16 16:57:51 +0900

    2File commit

--- src/main.java
+++ src/main.java
@@ -4,8 +4,14 @@ public class main {
 	public static void main(String[] args){
 		System.out.println("これは、サンプルプログラムです。");
 		printNum();
+
+		sample sample = new sample();
+		sample.writeCode();
 	}
 
+	/***
+	 * テストコードコメント追加
+	 */
 	public static void printNum(){
 		for(int i = 0 ; i < 10 ; i++){
 			System.out.println("i == " + i);
---
+++ src/sample.java
@@ -0,0 +1,10 @@
+
+public class sample {
+
+	/**
+	 * サンプルコード、API追加
+	 */
+	public void writeCode(){
+		System.out.println("サンプル、別API");
+	}
+}
