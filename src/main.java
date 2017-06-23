5d25e9c1d07a0e40333866cdf90c888353300a6b
nao-e <nao-e@192.168.100.129>
2017-06-23 18:51:51 +0900

    main

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ public class main {
 
 	public static void main(String[] args){
 		System.out.println("これは、サンプルプログラムです。");
+		System.out.println("これは、サンプルプログラムです。");
 		printNum();
 
 		sample sample = new sample();
